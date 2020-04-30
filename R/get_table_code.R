d37eb7298147c4e381230d46d36a7e5720fef925
s0521 <ss4057940@outlook.com>
2020-04-30 23:39:39 +0800

    初次调试成功

--- R/get_table_code.R
+++ R/get_table_code.R
@@ -6,7 +6,7 @@ get_table_code<- function(dataname = NULL, mptable_env = NULL,style=NULL) {
   }else{
 
     dcast_data_code<-get_dcast_data_code(dataname = dataname, mptable_env=mptable_env)
-    browser()
+    # browser()
     table_value<-eval(parse_expr(as.character(dcast_data_code)))
     flextable(table_value)
   }
@@ -22,17 +22,18 @@ get_dcast_data_code<- function(dataname = NULL, mptable_env = NULL){
   col_var<-c(mptable_env$Column)
   row_txt<-paste(collapse = " + ",row_var)
   col_txt<-paste(collapse = " + ",col_var)
-  formula<-paste(sep="",row_txt,col_txt)
+  formula<-paste(sep="~",row_txt,col_txt)
 
   fun.aggregate<-"list"
   value.var<-mptable_env$Data
 
-  paste(sep="",
+  dcast_data_code<-paste(sep="",
         "dcast(",
-        data_name,",",
+        data_name,", ",
         formula,",",
         "fun.aggregate =",fun.aggregate,",",
-        "value.var =",value.var,"",
+        "value.var =","'",value.var,"'","",
         ")"
         )
+  return(dcast_data_code)
 }
